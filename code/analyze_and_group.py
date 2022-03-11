42e82224c2e0aad9e44fb88862c47fde267b0b7e
amal-kadri <98130059+amal-kadri@users.noreply.github.com>
2022-03-10 19:31:12 -0600

    set up paths

--- code/analyze_and_group.py
+++ code/analyze_and_group.py
@@ -2,3 +2,12 @@
     each year for each county. Turn that data frame into a
     csv. Then aggregate the data into state level averages,
     and generate a csv for that state-level data set as well.'''
+
+import csv
+import os
+# cd into the project repo for the relative paths to work. For example:
+# cd /Users/amalkadri/Documents/GitHub/Housing_Price_Chloropleth/
+IN_PATH = os.path.join("artifacts", "zillow_merged.csv")
+OUTPUT_DIR = "artifacts"
+COUNTY_OUT_PATH = os.path.join(OUTPUT_DIR, "zillow_counties_final.csv")
+STATE_OUT_PATH = os.path.join(OUTPUT_DIR, "zillow_states_final.csv")
--- code/weight_and_merge.py
+++ code/weight_and_merge.py
@@ -1,2 +1,12 @@
 '''Apply population weighting to all counties and merge weight score
-    with cleaned zillow data set, then create merged csv'''
+    with cleaned zillow data set, then create merged data frame.
+    Then generate weighted average housing prices for later analysis'''
+
+import csv
+import os
+# cd into the project repo for the relative paths to work. For example:
+# cd /Users/amalkadri/Documents/GitHub/Housing_Price_Chloropleth/
+ZILLOW_IN_PATH = os.path.join("artifacts", "zillow_clean.csv")
+CENSUS_IN_PATH = os.path.join("artifacts", "census_clean.csv")
+OUTPUT_DIR = "artifacts"
+OUT_PATH = os.path.join(OUTPUT_DIR, "zillow_merged.csv")
