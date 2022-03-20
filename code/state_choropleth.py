5d5d363835ea30e755ad0abeae0da77a0c8072e8
KarloVlahek <karlovlahek@utexas.edu>
2022-03-20 14:54:03 -0500

    Created a dict reader.

--- code/state_choropleth.py
+++ code/state_choropleth.py
@@ -11,4 +11,19 @@ IN_PATH_2 = os.path.join("Data", "STATE_YEARLY.csv")
 OUTPUT_DIR = "Data"
 FINAL_MERGE_PATH = os.path.join(OUTPUT_DIR, "ST_Fips_Merge.csv")
 
+state_raw = pd.readcsv(IN_PATH_2)
+
+fips_dict = {}
+with open (IN_PATH) as in_file:
+    reader = csv.Dictreader(in_file)
+    for row in reader:
+        a = row['State']
+        b = row['FIPS State']
+        if a in fips_dict and row['State'] != 'NA':
+            pass
+        else:
+            try:
+                fips_dict[a] = b
+            except:
+                pass
 
