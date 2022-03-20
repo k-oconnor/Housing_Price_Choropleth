14f6eee522455cfce6c185e6f1782f5b2c778bf6
KarloVlahek <karlovlahek@utexas.edu>
2022-03-20 16:08:25 -0500

    Changed FIPS file and path, created CSV output with merged FIPS on STATE_YEARLY.csv.

--- code/state_choropleth.py
+++ code/state_choropleth.py
@@ -6,7 +6,7 @@ import pandas as pd
 import plotly.express as px
 import json
 
-IN_PATH = os.path.join("data", "US_FIPS_Codes.csv")
+IN_PATH = os.path.join("data", "us-state-ansi-fips.csv")
 IN_PATH_2 = os.path.join("data", "STATE_YEARLY.csv")
 OUTPUT_DIR = "Data"
 FINAL_MERGE_PATH = os.path.join(OUTPUT_DIR, "ST_Fips_Merge.csv")
@@ -16,10 +16,12 @@ state_raw = pd.read_csv(IN_PATH_2)
 fips_dict = {}
 with open (IN_PATH) as in_file:
     reader = csv.DictReader(in_file)
+    
     for row in reader:
-        a = row['State'] 
-        b = row['FIPS State']
-        if a in fips_dict and row['State'] != 'NA':
+    
+        a = row['stname'] 
+        b = row[' st']
+        if a in fips_dict and row['stname'] != 'NA':
             pass
         else:
             try:
