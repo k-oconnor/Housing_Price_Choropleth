07cfbf7f232bb6c649ce73a0e942b3b4f261c0b1
KarloVlahek <karlovlahek@utexas.edu>
2022-03-21 14:03:00 -0500

    changed the csv file again.

--- code/state_choropleth.py
+++ code/state_choropleth.py
@@ -6,10 +6,10 @@ import pandas as pd
 import plotly.express as px
 import json
 
-IN_PATH = os.path.join("data", "us-state-ansi-fips.csv")
-IN_PATH_2 = os.path.join("data", "STATE_YEARLY.csv")
+IN_PATH = os.path.join("data", "US_FIPS_codes.csv")
+IN_PATH_2 = os.path.join("data", "STATE_AGGREGATE.csv")
 OUTPUT_DIR = "Data"
-FINAL_MERGE_PATH = os.path.join(OUTPUT_DIR, "ST_Fips_Merge.csv")
+FINAL_MERGE_PATH = os.path.join(OUTPUT_DIR, "STAGG_Fips_Merge.csv")
 
 state_raw = pd.read_csv(IN_PATH_2)
 
@@ -17,17 +17,19 @@ fips_dict = {}
 with open (IN_PATH) as in_file:
     reader = csv.DictReader(in_file)
     
-    for row in reader:
+    # for row in reader:
     
-        a = row['stname'] 
-        b = row[' st']
-        if a in fips_dict and row['stname'] != 'NA':
-            pass
-        else:
-            try:
-                fips_dict[a] = b
-            except:
-                pass
+    #     a = row['ï»¿State'] 
+    #     b = row['FIPS State']
+    #     if a in fips_dict and row['ï»¿State'] != 'NA':
+    #         pass
+    #     else:
+    #         try:
+    #             fips_dict[a] = b
+    #         except:
+    #             pass
 
-state_raw['FIPS'] = state_raw['STNAME'].map(fips_dict)
-state_raw.to_csv(FINAL_MERGE_PATH)
+print(reader)
+
+# state_raw['FIPS'] = state_raw['STNAME'].map(fips_dict)
+# state_raw.to_csv(FINAL_MERGE_PATH)
