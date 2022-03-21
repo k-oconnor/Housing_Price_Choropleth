f49d0d6548e7f8cbba255d696f181bf47b1054b4
KarloVlahek <karlovlahek@utexas.edu>
2022-03-21 15:12:37 -0500

    PC County Merge

--- code/County_Fips_Merge.py
+++ code/County_Fips_Merge.py
@@ -20,25 +20,26 @@ fips_dict = {}
 with open(IN_PATH) as in_file:
     reader = csv.DictReader(in_file)
     for row in reader:
-        a = (row["ï»¿State"], row["County Name"] + " County")
-        b = row["FIPS State"]
-        c = row["FIPS County"]
-        if a not in fips_dict and row["County Name"] != "NA":
-            fips_dict[a] = b + c
+        print(row)
+#         a = (row["ï»¿State"], row["County Name"] + " County")
+#         b = row["FIPS State"]
+#         c = row["FIPS County"]
+#         if a not in fips_dict and row["County Name"] != "NA":
+#             fips_dict[a] = b + c
 
-keys = list(fips_dict.keys())
-values = list(fips_dict.values())
+# keys = list(fips_dict.keys())
+# values = list(fips_dict.values())
 
-key_list = []
+# key_list = []
 
-for i in range(len(keys)):
-    key_list += [list(keys[i]) + [values[i]]]
+# for i in range(len(keys)):
+#     key_list += [list(keys[i]) + [values[i]]]
 
-key_frame = pd.DataFrame(key_list, columns=["STNAME", "CTYNAME", "FIPS"])
+# key_frame = pd.DataFrame(key_list, columns=["STNAME", "CTYNAME", "FIPS"])
 
-county_merge = pd.merge(key_frame, county_raw, on=["STNAME", "CTYNAME"])
+# county_merge = pd.merge(key_frame, county_raw, on=["STNAME", "CTYNAME"])
 
-county_merge_ag = pd.merge(key_frame, county_ag, on=["STNAME", "CTYNAME"])
+# county_merge_ag = pd.merge(key_frame, county_ag, on=["STNAME", "CTYNAME"])
 
-county_merge.to_csv(FINAL_MERGE_PATH)
-county_merge_ag.to_csv(FINAL_MERGE_PATH_AG)
+# county_merge.to_csv(FINAL_MERGE_PATH)
+# county_merge_ag.to_csv(FINAL_MERGE_PATH_AG)
